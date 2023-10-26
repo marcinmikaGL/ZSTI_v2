d45f0d389f6ffbfb8002cf42b0d8b8ae8cb07d8b
marcinmika <marcinmika@o2.pl>
2023-10-26 20:53:57 +0200

    Update skrypt.js

--- lekcja7/skrypt.js
+++ lekcja7/skrypt.js
@@ -8,3 +8,12 @@ const roznicaWMillisekundach = dataKoncowa - dataPoczatkowa;
 const roznicaWSekundach = roznicaWMillisekundach / 1000;
 
 console.log("Różnica w sekundach: " + roznicaWSekundach);
+
+
+function data(dane) {
+ let skladnikiDaty = dane.split(".");
+ let dzien = parseInt(skladnikiDaty[0], 10);
+ let miesiac = parseInt(skladnikiDaty[1], 10) - 1; // Miesiące w JavaScript są indeksowane od zera, więc odejmujemy 1
+ let rok = parseInt(skladnikiDaty[2], 10);
+ return  new Date(rok, miesiac, dzien);
+}

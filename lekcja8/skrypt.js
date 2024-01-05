3f334f32d19c8eafa3464d29fa3af59c2de79a49
marcinmika <marcinmika@o2.pl>
2024-01-05 11:20:18 +0100

    Update skrypt.js

--- lekcja8/skrypt.js
+++ lekcja8/skrypt.js
@@ -1,1 +1,22 @@
 
+tablica = [
+  [1,2,3,"kopytko"],
+  [1,3,5,"pierogi"],
+  [6,6,6,"racuchy"],
+];
+i = 0;
+let tab;
+tab2 = {};
+jestemRekurancja(tablica);
+
+function jestemRekurancja(tab) {
+  if(tab === undefined) { 
+    return;
+  } else {
+    i++;
+    tab2[i] = {key:"a"+i, value:tab[i] }; 
+    jestemRekurancja(tab[i]);
+  }
+}
+console.log(tab2);
+

4f36afb226dffe259b3d3dca8e3d5897376be987
marcinmika <marcinmika@o2.pl>
2024-01-05 11:24:36 +0100

    Update skrypt2.js

--- lekcja8/skrypt2.js
+++ lekcja8/skrypt2.js
@@ -1,21 +1,34 @@
 function wyswietlTabliceRekurencyjnie(tablica) {
   for (let i = 0; i < tablica.length; i++) {
     if (Array.isArray(tablica[i])) {
-      // JeÅli element jest tablicÄ, wywoÅaj funkcjÄ rekurencyjnie
       wyswietlTabliceRekurencyjnie(tablica[i]);
     } else {
-      // JeÅli element nie jest tablicÄ, wyÅwietl jego wartoÅÄ
       console.log(tablica[i]);
     }
   }
 }
 
-// PrzykÅadowa tablica podwÃ³jnie zÅoÅ¼ona
 const mojaTablica = [
   [1, 2, 3],
   [4, [5, 6], 7],
   8,
 ];
 
-// WywoÅanie funkcji rekurencyjnej
+wyswietlTabliceRekurencyjnie(tablica);
+function wyswietlTabliceRekurencyjnie(tablica) {
+  for (let i = 0; i < tablica.length; i++) {
+    if (Array.isArray(tablica[i])) {
+      wyswietlTabliceRekurencyjnie(tablica[i]);
+    } else {
+      console.log(tablica[i]);
+    }
+  }
+}
+
+const mojaTablica = [
+  [1, 2, 3],
+  [4, [5, 6], 7],
+  8,
+];
+
 wyswietlTabliceRekurencyjnie(tablica);

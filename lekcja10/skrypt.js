d029b2caf001ff612171577909fea59b263bcd1a
marcinmika <marcinmika@o2.pl>
2024-01-12 08:45:28 +0100

    Update skrypt.js

--- lekcja10/skrypt.js
+++ lekcja10/skrypt.js
@@ -1,1 +1,31 @@
+const Osoba = function(imie, nazwisko, wiek) {
+  this.imie = imie;
+  this.nazwisko = nazwisko;
+  this.wiek = wiek;
+
+  this.toString = function() {
+    return `Imię: ${this.imie}, nazwisko: ${this.nazwisko}, wiek: ${this.wiek}`;
+  };
+};
+
+const osoba = new Osoba("Jan", "Kowalski", 30);
+
+console.log(osoba.toString());
+
+const Osoba = function(imie, nazwisko, wiek) {
+  this.imie = imie;
+  this.nazwisko = nazwisko;
+  this.wiek = wiek;
+};
+
+Object.defineProperty(Osoba.prototype, "toString", {
+  value: function() {
+    return `Imię: ${this.imie}, nazwisko: ${this.nazwisko}, wiek: ${this.wiek}`;
+  },
+});
+
+const osoba = new Osoba("Jan", "Kowalski", 30);
+
+console.log(osoba.toString());
+
 
